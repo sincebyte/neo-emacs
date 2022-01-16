0327a5c1af43cda6d407a7bba1b9408b6efd86f2
vanniuner <vanniuner@qq.com>
2022-01-16 22:27:49 +0800

    fix: private dir

--- config.el
+++ config.el
@@ -7,7 +7,7 @@
       org-directory               "~/org/"                                                 ;; org           root path
       org-roam-directory          "~/org/org-roam"                                         ;; org roam      root path
       lsp-java-java-path          (concat (getenv "JAVA_HOME") "/bin/java")                ;; java11        exec path
-      user-private-dir            "~/org/org-roam/doom/config/"                            ;; user private dir
+      user-private-dir            "~/org/org-roam/command/doom/config/"                            ;; user private dir
       doom-font                   (font-spec :family "等距更纱黑体 Slab SC" :size 18)      ;; font setting
       display-line-numbers-type   nil                                                      ;; show line number
       rime-user-data-dir          "~/Library/Rime/"                                        ;; rime config input method setting
