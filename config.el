e2f9d3b0e0298f665ebedd71c112501ca46d5c6c
vanniuner <vanniuner@qq.com>
2022-05-04 00:30:47 +0800

    add: ejc-sql query detail

--- config.el
+++ config.el
@@ -27,3 +27,34 @@
 ;; (use-package pulsing-cursor
 ;;    :config (pulsing-cursor-mode +1))
 
+
+;; for ejc-sql query detail
+(defvar ejc-results-detail-buffer nil
+  "The results detail buffer.")
+
+(defun goto-result-detail ()
+  (interactive)
+  (let ((list-data (split-string (buffer-substring-no-properties (line-beginning-position) (line-end-position)) " | " ))
+        (max-head-length 0)
+        (c-point (point))
+        (list-head (split-string (buffer-substring-no-properties 1 (search-backward "\n" nil t (- (string-to-number(elt (split-string (what-line) " ") 1 )) 1 ) )) " | ")) )
+        ;; (line-head (buffer-substring-no-properties 1 239)))
+    (goto-char c-point)
+  (when (not (and ejc-results-detail-buffer (buffer-live-p ejc-results-detail-buffer)))
+    (setq ejc-results-detail-buffer (get-buffer-create "*ejc-results-detail-buffer*"))
+  (with-current-buffer ejc-results-detail-buffer (ejc-result-mode)))
+  ;; (print (string-to-number(elt (split-string (what-line) " ") 1 )))
+  ;; (print (search-backward "\n" nil t (- (string-to-number(elt (split-string (what-line) " ") 1 )) 1 ) ))
+  (switch-to-buffer ejc-results-detail-buffer)
+  (erase-buffer)
+  (cl-loop
+   for element in list-head
+   do (if (< max-head-length (length element))
+          (setq max-head-length (length element))))
+  (cl-loop
+    for i from 0 to (- (length list-head) 1)
+    do (with-current-buffer ejc-results-detail-buffer (insert
+        (elt list-head i)
+        (string-join (make-list (- max-head-length (length (elt list-head i)) ) " ") "")
+        "| " (elt list-data i) "\n"))
+   )))
