7b56bc0b0625da61f21d330450e3a8091f8b65a7
goldkinn-liu.zijie <lzj@gaojin.com.cn>
2022-11-02 22:30:27 +0800

    fix readme

--- config.el
+++ config.el
@@ -14,6 +14,7 @@
       rime-user-data-dir          "~/Library/Rime/"                                        ;; rime config input method setting
       rime-librime-root           "~/.doom.d/neoemacs/rime-macos/dist"                     ;; emacs-rime/blob/master/INSTALLATION.org
       emacs-module-root           "/opt/homebrew/opt/emacs-plus@28/include"                ;; emcas exec path
+      rime-emacs-module-header-root "/Applications/Emacs.app/Contents/Resources/include"   ;; for emacs rime, brew do not needed
       lsp-java-jdt-download-url   "https://download.eclipse.org/jdtls/milestones/1.9.0/jdt-language-server-1.9.0-202203031534.tar.gz" ;; jdt-server URL,with tencent cloud speed up
 )
 ;; default core setting
