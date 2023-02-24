49c6c7ea618e8aacfb5400b160f016a2b21eea35
vanniuner <at18708169062@gmail.com>
2023-02-24 21:08:53 +0800

    fix: doom modeline buffer file

--- neoemacs.el
+++ neoemacs.el
@@ -120,7 +120,7 @@
       lsp-idle-delay                             0.01
       ejc-result-table-impl                      'ejc-result-mode
       gts-translate-list                         '(("en" "zh"))
-      doom-modeline-buffer-file-name-style       'file-name  )
+      doom-modeline-buffer-file-name-style       'truncate-all  )
 
 ;; (display-time)
 (setq package-archives '(( "gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/"   )
