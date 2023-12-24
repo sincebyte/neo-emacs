eaec68a05a5f1e8c7ed7c858406ab71a382d1c53
vanniuner <sincebyte@gmail.com>
2023-12-24 21:15:34 +0800

    Update config.el

--- modules/neo-emacs/common/config.el
+++ modules/neo-emacs/common/config.el
@@ -1,5 +1,6 @@
 (defun doom-dashboard-widget-footer () "For empty element." (insert ""))
 (setq fancy-splash-image (concat doom-user-dir "logo.png"))
 (set-default 'truncate-lines nil   )
+(setq-default with-editor-emacsclient-executable "emacsclient")
 
 (global-set-key (kbd "<RET>" ) 'evil-ret )
