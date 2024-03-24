aab922c0c99d9dca7d17a9f080b19677cbe511f8
liu.zijie <at18708169062@gmail.com>
2024-03-24 14:28:51 +0800

    feat: set proxy code template

--- neoemacs/db-work.el
+++ neoemacs/db-work.el
@@ -14,5 +14,13 @@
                          :connection-uri "jdbc:mysql://localhost:3306/spring?useSSL=false&user=root&password=root&characterEncoding=utf8"
                          :separator      "</?\.*>" )
   )
+;; set proxy
+(defun convert-to-regex (str)
+  (let* ((joined (replace-regexp-in-string "," "\\\\|" str))
+         (wrapped (concat "^\\(" joined "\\)")))
+    wrapped))
+(setq url-proxy-services `(("http" ."vpn.local.com:10887")
+                           ("https" ."vpn.local.com:10887")
+                           ("no_proxy" . ,(convert-to-regex ".*aliyun.com,192.168.0.0/16,137.239.155.184"))))
 (provide 'db-work)
 ;;; db-work.el ends here
