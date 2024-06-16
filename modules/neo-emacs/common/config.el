d2ce2d8f77ec0f10e069b3c4812278516d8562c9
liu.zijie <at18708169062@gmail.com>
2024-06-16 22:41:55 +0800

    fix: gc

--- modules/neo-emacs/common/config.el
+++ modules/neo-emacs/common/config.el
@@ -11,6 +11,9 @@
 (prefer-coding-system 'utf-8-unix)
 (setq-default buffer-file-coding-system 'utf-8-unix)
 
+(setq gc-cons-threshold 100000000) ; Increase garbage collection threshold
+(setq read-process-output-max (* 1024 1024)) ; Increase the
+
 (menu-bar-mode -1)
 (keyfreq-mode 1)
 (keyfreq-autosave-mode 1)
