e6cbb78abb6a191e7b3044d12554807fc418d6eb
liu.zijie <at18708169062@gmail.com>
2026-01-02 00:00:58 +0800

    chore: 更新 rime 配置与快捷键绑定

--- modules/neo-emacs/translate/config.el
+++ modules/neo-emacs/translate/config.el
@@ -1,30 +1,12 @@
 ;;; neoemacs/translate/config.el -*- lexical-binding: t; -*-
 
 (use-package! gt :defer t               )
-
-                                        ;(setq gt-langs '(en zh))
-                                        ;(setq gt-default-translator (gt-translator :engines (gt-youdao-dict-engine)))
 (map! :ne "; t"     'gt-translate )
 (map! :ve "; t"     'gt-translate )
-;; (map! :ne "; y"     'gt-start gbuffer)
-;;(map! :ne "; y"     'gt-start gt-buffer)
-;; (map! :ne "; y"     'gt-go-buffer-translate)
-
-;; (map! :ne "; y"     'gt-do-translate-buffer     )
-
-(map! :after gt 
-      :map evil-normal-state-map
-      "q" nil)
 
 (after! gt
   (setq gt-langs '(en zh))
-  (setq gt-default-translator (gt-translator :engines (gt-youdao-dict-engine)))
-  (setq gt-preset-translators
-        `((default . ,(gt-translator
-                       :taker   (gt-taker :text 'word :pick 'paragraph)
-                       :engines (list (gt-youdao-dict-engine))
-                       :render  (gt-render)))
-          (gbuffer . ,(gt-translator
-                       :taker   (gt-taker :text 'word :pick 'paragraph)
-                       :engines (list (gt-youdao-dict-engine))
-                       :render  (gt-buffer-render))))))
+  (setq gt-default-translator
+      (gt-translator :engines (gt-youdao-dict-engine)
+                     :render (list (gt-insert-render  :if 'not-word )
+                                   (gt-posframe-pop-render :if 'word     )))))
