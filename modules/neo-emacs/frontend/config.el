60da1aa93f506598a94862f89d528254ba9efa38
liu.zijie <at18708169062@gmail.com>
2026-04-01 14:49:55 +0800

    feat: add vue lsp support

--- modules/neo-emacs/frontend/config.el
+++ modules/neo-emacs/frontend/config.el
@@ -26,3 +26,27 @@
   (setq lsp-disabled-clients '(deno-ls)))
 (after! css-mode
   (setq css-indent-offset 2))
+
+(defun my-vue-indent-setup ()
+  (setq web-mode-markup-indent-offset 2)   ;; template/html
+  (setq web-mode-css-indent-offset 2)      ;; style
+  (setq web-mode-code-indent-offset 2)     ;; script/js
+  (setq indent-tabs-mode nil)              ;; 使用空格
+  ;; 额外指定 JS/TS 缩进
+  (setq js-indent-level 2)
+  (setq typescript-indent-level 2)
+  ;; 禁用 electric-indent 避免 o/ O 自动缩进异常
+  (electric-indent-local-mode -1))
+
+(add-hook 'web-mode-hook 'my-vue-indent-setup)
+
+(after! lsp-mode
+  ;; 自动开启 lsp
+  (add-hook 'vue-mode-hook #'lsp)
+
+  ;; 忽略 Volar 的一些提示（可选）
+  (setq lsp-vue-language-server-command '("vue-language-server" "--stdio"))
+  ;; 延迟请求，避免超时
+  (setq lsp-idle-delay 0.500)
+  ;; 禁用不必要的额外文本编辑
+  (setq lsp-completion-enable-additional-text-edit nil))
