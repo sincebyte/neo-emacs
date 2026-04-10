89ee6f2e62a341d15cc6970361e78abbb045f76a
liu.zijie <at18708169062@gmail.com>
2026-04-10 22:40:29 +0800

    fix: corfu

--- modules/neo-emacs/java/config.el
+++ modules/neo-emacs/java/config.el
@@ -42,18 +42,25 @@
   :after corfu
   :custom
   (kind-icon-blend-background t)
-                                        ; (kind-icon-default-face 'corfu-default) ; only needed with blend-background
   :config
   (setq kind-icon-use-icons  nil
+        kind-icon-blend-background nil
         corfu-count          7    )
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
 (after! corfu
   (corfu-popupinfo-mode -1)              ; 强制禁用
+  (set-face-attribute 'corfu-current nil :weight 'normal)
+  (set-face-attribute 'corfu-default nil :background (face-background 'corfu-border nil t))
+  (set-face-attribute 'corfu-border nil :background (face-background 'org-mode-line-clock nil t))
+  (add-to-list 'corfu--frame-parameters '(vertical-scroll-bars . nil))
   (setq corfu-popupinfo-delay nil        ; 禁止延迟触发
         corfu-popupinfo-hide t           ; 隐藏可能残留的弹窗
+        corfu-right-margin-width  0.3
+        corfu-border-width        2
         corfu-auto                t
         corfu-preselect           'first
-        corfu-bar-width           0.3
+        corfu-bar-width           0.5
         corfu-quit-at-boundary    nil
         corfu-quit-no-match       nil
         corfu-popupinfo-max-width 0      ; 确保弹窗尺寸为零
